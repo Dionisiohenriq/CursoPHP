becfb1d151bfcfdf79d26ec2767f4c097c840b1a
Henrique Dionisio Ferreira <henriquedionisio.dionisioferre@gmail.com>
2021-03-27 04:14:00 -0300

    Atualização de exercícios

--- index.php
+++ index.php
@@ -15,80 +15,17 @@
     </header>
     <main class="principal">
         <div class="conteudo">
-            <div class="modulos">
+            <nav class="modulos">
                 <div class="modulo verde">
                     <h3>Módulo 01</h3>
                     <ul>
-                        <li><a href="exercicio.php">Exercício A</a></li>
-                        <li><a href="#">Exercício B</a></li>
-                        <li><a href="#">Exercício C</a></li>
+                        <li><a href="exercicio.php?dir=basico&file=ola">Olá PHP</a></li>
                     </ul>
-                </div>
-                <div class="modulo vermelho">
-                    <h3>Módulo 02</h3>
                     <ul>
-                        <li><a href="#">Exercício A</a></li>
-                        <li><a href="#">Exercício B</a></li>
-                        <li><a href="#">Exercício C</a></li>
+                        <li><a href="exercicio.php?dir=basico&file=html">Integração Html</a></li>
                     </ul>
                 </div>
-                <div class="modulo azul">
-                    <h3>Módulo 03</h3>
-                    <ul>
-                        <li><a href="#">Exercício A</a></li>
-                        <li><a href="#">Exercício B</a></li>
-                        <li><a href="#">Exercício C</a></li>
-                    </ul>
-                </div>
-                <div class="modulo pink">
-                    <h3>Módulo 04</h3>
-                    <ul>
-                        <li><a href="#">Exercício A</a></li>
-                        <li><a href="#">Exercício B</a></li>
-                        <li><a href="#">Exercício C</a></li>
-                    </ul>
-                </div>
-                <div class="modulo violeta">
-                    <h3>Módulo 05</h3>
-                    <ul>    
-                        <li><a href="#">Exercício A</a></li>
-                        <li><a href="#">Exercício B</a></li>
-                        <li><a href="#">Exercício C</a></li>
-                    </ul>
-                </div>
-                <div class="modulo amarelo">
-                    <h3>Módulo 06</h3>
-                    <ul>
-                        <li><a href="#">Exercício A</a></li>
-                        <li><a href="#">Exercício B</a></li>
-                        <li><a href="#">Exercício C</a></li>
-                    </ul>
-                </div>
-                <div class="modulo turqueza">
-                    <h3>Módulo 07</h3>
-                    <ul>
-                        <li><a href="#">Exercício A</a></li>
-                        <li><a href="#">Exercício B</a></li>
-                        <li><a href="#">Exercício C</a></li>
-                    </ul>
-                </div>
-                <div class="modulo marrom">
-                    <h3>Módulo 08</h3>
-                    <ul>
-                        <li><a href="#">Exercício A</a></li>
-                        <li><a href="#">Exercício B</a></li>
-                        <li><a href="#">Exercício C</a></li>
-                    </ul>
-                </div>
-                <div class="modulo roxo">
-                    <h3>Módulo 09</h3>
-                    <ul>
-                        <li><a href="#">Exercício A</a></li>
-                        <li><a href="#">Exercício B</a></li>
-                        <li><a href="#">Exercício C</a></li>
-                    </ul>
-                </div>
-            </div>
+            </nav>
         </div>
     </main>
     <footer class="rodape">
